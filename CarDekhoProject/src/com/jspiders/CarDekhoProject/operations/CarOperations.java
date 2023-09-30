b5b6ab7e47edab5f4661850cb95858d3aaa6b75c
MohitKhodse44 <118118320+MohitKhodse44@users.noreply.github.com>
2023-09-30 22:59:41 +0530

    CarOperations.java

--- CarDekhoProject/src/com/jspiders/CarDekhoProject/operations/CarOperations.java
+++ CarDekhoProject/src/com/jspiders/CarDekhoProject/operations/CarOperations.java
@@ -10,38 +10,38 @@ public class CarOperations {
         carList = new ArrayList<>();
     }
 
-    // Add a new car to the list
+   
     public void addCar(Car car) {
         carList.add(car);
     }
 
-    // Update an existing car's details
+    
     public void updateCar(int carId, Car updatedCar) {
         for (int i = 0; i < carList.size(); i++) {
             Car car = carList.get(i);
             if (car.getId() == carId) {
                 carList.set(i, updatedCar);
-                return; // Exit the loop after updating
+                return; 
             }
         }
     }
 
-    // Remove a car from the list by ID
+    
     public void removeCar(int carId) {
         carList.removeIf(car -> car.getId() == carId);
     }
 
-    // Retrieve a car by ID
+   
     public Car retrieveCar(int carId) {
         for (Car car : carList) {
             if (car.getId() == carId) {
-                return car; // Return the car if found
+                return car; 
             }
         }
-        return null; // Return null if not found
+        return null; 
     }
 
-    // Get a list of all cars
+    
     public List<Car> getAllCars() {
         return new ArrayList<>(carList);
     }
